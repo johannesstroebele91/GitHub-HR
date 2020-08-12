fe0b3d5d6e7bdf32e4f7a82828becb370fc99eed
JohannesStroebele <stroebele.j@gmail.com>
2020-08-12 11:12:55 +0200

    Added login component and fixed minor issues

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+import {LoginCredentials} from '../../models/login-credentials';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  submitted = false;
+  showSubmitError = false;
+  testLogin = new LoginCredentials('test@gmail.com', 'password');
+
+  constructor(private router: Router) {
+    this.testLogin.email = '';
+    this.testLogin.password = '';
+  }
+
+  onSubmit() {
+    if (this.testLogin.email === 'test@gmail.com' && this.testLogin.password === 'password') {
+      console.log(this.testLogin.email);
+      console.log(this.testLogin.password);
+      this.submitted = true;
+      this.showSubmitError = false;
+      this.router.navigate(['/home']);  // define your component where you want to go
+
+    } else {
+      console.log('if');
+      this.showSubmitError = true;
+    }
+  }
+
+  registerUser() {
+
+  }
+}
