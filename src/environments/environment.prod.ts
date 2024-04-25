320d3f1130eb03dd25e88867f82a2c97ba3f704e
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-25 08:49:54 +0200

    Improve styling for overall github user profile and repos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  clientId: 'a3b037c468909cdc00c5', // needs to be set manually for running the application locally
-  clientSecret: '40b1f3890764ef7553d2c87ae3a284231d0ab6a8', // needs to be set manually for running the application locally
+  clientId: 'a3b037c468909cdc00c5',
+  clientSecret: '40b1f3890764ef7553d2c87ae3a284231d0ab6a8',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   clientId: 'a3b037c468909cdc00c5', // needs to be set manually for running the application locally
   clientSecret: '40b1f3890764ef7553d2c87ae3a284231d0ab6a8', // needs to be set manually for running the application locally
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
